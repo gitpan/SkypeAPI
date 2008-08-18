320c9a31f58310fd7b9b8ef73836d23cd266429b
xiaoshengcaicai <laomoi@cpan.org>
2008-08-18 02:16:04 +0000

    Import of LAOMOI/SkypeAPI-0.03 from CPAN.
    gitpan-cpan-distribution: SkypeAPI
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         LAOMOI/SkypeAPI-0.03.tar.gz
    gitpan-cpan-author:       LAOMOI
    gitpan-cpan-maturity:     released

--- lib/SkypeAPI.pm
+++ lib/SkypeAPI.pm
@@ -1,13 +1,13 @@
 package SkypeAPI;
 
-use 5.008008;
+use 5.008005;
 use strict;
 use warnings;
 use Digest::MD5;
 require Exporter;
 
 our @ISA = qw(Exporter);
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 require XSLoader;
 XSLoader::load('SkypeAPI', $VERSION);
